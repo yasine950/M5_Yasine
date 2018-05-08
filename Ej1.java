aef7c87835d80f52eda3c18ca19c7ffc44797475
Repo de Yasine <yasine.950@gmail.com>
2018-05-08 11:10:42 +0200

    Arxiu cambiat

--- Ej1.java
+++ Ej1.java
@@ -4,14 +4,6 @@ import java.util.Scanner;
 
 public class Ej1 {
 	public static void main(String[] args) {
-	int dni;
-	String letras[]=chimpanze
-	Scanner scan=new Scanner(System.in);
-	System.out.println("Introduce tu DNI");
-	dni=scan.nextInt();
-	int posicio=dni%23;
-	System.out.println("Tu letra es: "+letras[posicio]);
-	System.out.println("NIF: "+dni+"-"+letras[posicio]);
-	scan.close();
+		Exercici de Yasine cambiat per Acram
 	}
 }
