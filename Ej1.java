655c2a22ce54e97b119ae377d416e98b53f85597
yasine950 <39044920+yasine950@users.noreply.github.com>
2018-05-08 12:48:35 +0200

    Add files via upload

---
+++ Ej1.java
@@ -0,0 +1,17 @@
+package ams1_yasine_kahouaji_Vectors3;
+
+import java.util.Scanner;
+
+public class Ej1 {
+	public static void main(String[] args) {
+	int dni;
+	String letras[]={"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Introduce tu DNI");
+	dni=scan.nextInt();
+	int posicio=dni%23;
+	System.out.println("Tu letra es: "+letras[posicio]);
+	System.out.println("NIF: "+dni+"-"+letras[posicio]);
+	scan.close();
+	}
+}
