2367c3706cdcda9b9dbace602855377e59f540fc
Repo de Yasine <yasine.950@gmail.com>
2018-05-08 10:53:03 +0200

    Confirmat

--- Ej1.java
+++ Ej1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ej1 {
 	public static void main(String[] args) {
 	int dni;
-	String letras[]={"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+	String letras[]=chimpanze
 	Scanner scan=new Scanner(System.in);
 	System.out.println("Introduce tu DNI");
 	dni=scan.nextInt();
